34dd1bc4daf508f9f58d0b817f825d9ea4d1e39f
Tirtha-Bhatta <tirthabhatta2060@gmail.com>
2026-06-17 15:04:26 +0530

    Time: 3 ms (13.02%), Space: 43.5 MB (41.29%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) {
+        //bubble sort concept logic
         for(int i=0;i<nums.length;i++){
             for(int j=0;j<nums.length-i-1;j++){
                 if(nums[j]>nums[j+1]){
